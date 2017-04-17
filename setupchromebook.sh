3be8905b9d6902ec10c4e2fb7ac0f5629c7e1c51
Henry Collingridge <henry@collingridge.net>
2017-04-17 20:40:18 +1200

    fixed cerebrus install, silenced apt-get

--- setupchromebook.sh
+++ setupchromebook.sh
@@ -10,11 +10,11 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 #Install package:
 sudo apt-get update 
-sudo apt-get install google-chrome-stable
+sudo apt-get install -q google-chrome-stable
 
 echo install cerebro
 
-wget --quiet --output-document=- https://github.com/KELiON/cerebro/releases/download/v0.2.8/cerebro_0.2.8_amd64.deb | dpkg -- install -
+wget --quiet --output-document=temp "https://github.com/KELiON/cerebro/releases/download/v0.2.8/cerebro_0.2.8_amd64.deb" ; dpkg --install temp
 
 echo chromebook fixes and tweaks
 curl -LOk https://github.com/fascinatingcaptain/CBFixesAndTweaks/archive/master.tar.gz; tar -zxvf master.tar.gz; cd CBFixesAndTweaks-master; sudo -E bash CBFixesAndTweaks.sh
